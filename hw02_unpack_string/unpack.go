14fd5f8d45a64d5ea3d515ae1e9777f1c84dc573
rokku3kpvc <ykwnd@yahoo.com>
2021-11-22 01:25:30 +0500

    Выполнено домашнее задание №2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,42 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var res strings.Builder
+	var prev rune
+
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			if prev == 0 {
+				return "", ErrInvalidString
+			}
+
+			num, err := strconv.Atoi(string(r))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			repeat := strings.Repeat(string(prev), num)
+			res.WriteString(repeat)
+			prev = 0
+		} else {
+			if prev != 0 {
+				res.WriteString(string(prev))
+			}
+			prev = r
+		}
+	}
+
+	if prev != 0 {
+		res.WriteString(string(prev))
+	}
+
+	return res.String(), nil
 }
